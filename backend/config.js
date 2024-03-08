374d0a08cec2795b3145823cb88c61d2656562c1
Witek Pruchnicki <witekpruchnicki@gmail.com>
2024-03-08 18:29:12 +0100

    Frontend build

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,6 @@ export const mongoDBURL =
   'mongodb+srv://root:12345@cluster-merito.sgb0lbk.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster-Merito';
 
 // Please create a free database for yourself.
-// This database will be deleted after tutorial
+// This database will be deleted after tutoria
 
 export const baseUrl="https://fullstackazure-app.azurewebsites.net/"
